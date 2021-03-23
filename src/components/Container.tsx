eaacfdc15838dfbcbff97e4ca78441a9226eacfa
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-23 19:48:39 -0300

    refact: create container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "../styles/container.css"
+
+const Container: React.FC = () => {
+  return <div className="container">
+
+  </div>;
+}
+
+export default Container;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { Content } from "./Content";
+
 export function SideBar() {
-  // Complete aqui
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+
+      <Content/>
+    </div>
+  )
 }
